9cd56ee89c22daae1a61d070a4f4f789992417b9
Nathan Wick <nathanwick2001@gmail.com>
2022-11-16 21:34:22 -0500

    Init Firestore

---
+++ sprost/src/components/Database.tsx
@@ -0,0 +1,17 @@
+import { FirebaseApp } from "firebase/app";
+import { Firestore, getFirestore } from "firebase/firestore";
+import { createContext, useContext } from "react";
+import { FirebaseContext } from "./Firebase";
+
+export const DatabaseContext = createContext<Firestore | undefined>(undefined);
+
+const DatabaseContextProvider = (props: any) => {
+    const firebaseApp = useContext(FirebaseContext);
+    const database = getFirestore(firebaseApp as FirebaseApp);
+
+    return <DatabaseContext.Provider value={database}>
+        {props.children}
+    </DatabaseContext.Provider>
+};
+
+export default DatabaseContextProvider;
