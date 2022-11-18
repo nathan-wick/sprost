942e0cc713a8c91d1bfc05773f576a6d470b0c60
Nathan Wick <nathanwick2001@gmail.com>
2022-11-17 20:59:30 -0500

    Get User From Firestore

--- sprost/src/components/sprost/views/Account.tsx
+++ sprost/src/components/sprost/views/Account.tsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { UserContext } from "../../User";
 import SignOut from "../SignOut";
 
 const Account = () => {
+    const user = useContext(UserContext);
+
     return <>
     <h1>Account</h1>
+    <p>{user?.name}</p>
     <SignOut />
 </>;
 }
