fc4fc0d0e196d111bf5a66ad890d6ecf2a8c4aee
Nathan Wick <nathanwick2001@gmail.com>
2022-11-18 21:41:05 -0500

    Create Information Modal

---
+++ sprost/src/components/sprost/modals/Information.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { InfoCircle } from "react-bootstrap-icons";
+
+const Information = (props: any) => {
+    const [ modal, setModal ] = useState<boolean>(false);
+    const showModal = () => setModal(true);
+    const hideModal = () => setModal(false);
+    
+    return <>
+        <Button 
+            variant="link"
+            size="sm"
+            disabled={modal}
+            onClick={showModal}>
+                <InfoCircle
+                    className="mx-2" />
+        </Button>
+
+        <Modal show={modal} onHide={hideModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>
+                    {props.title}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {props.text}
+            </Modal.Body>
+        </Modal>
+    </>
+}
+
+export default Information;
