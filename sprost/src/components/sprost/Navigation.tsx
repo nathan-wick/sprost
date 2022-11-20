ce92f9e30491b9614ff6b98e994281f45746af4a
Nathan Wick <nathanwick2001@gmail.com>
2022-11-20 16:53:14 -0500

    Create Themes

--- sprost/src/components/sprost/Navigation.tsx
+++ sprost/src/components/sprost/Navigation.tsx
@@ -1,27 +1,37 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { PersonCircle, Speedometer, WindowStack } from "react-bootstrap-icons";
+import { UserContext } from "../User";
 import Account from "./views/Account";
 import Apps from "./views/Apps";
 import Dashboard from "./views/Dashboard";
 
 const Navigation = () => {
+  const user = useContext(UserContext);
   const [currentView, setCurrentView] = useState<JSX.Element>(<Dashboard />);
 
   // TODO: Replace Navbar with Sidebar
 
   const oldNav = <>
-    <Navbar bg="light" expand="lg">
+    <Navbar
+      bg={user?.theme.name === `dark` ? `black` : `white`}
+      expand="lg">
       <Container>
-        <Navbar.Brand onClick={() => setCurrentView(<Dashboard />)}>
+        <Navbar.Brand
+          className={user?.theme.name === `dark` ? `text-light` : `text-dark`}
+          onClick={() => setCurrentView(<Dashboard />)}>
           Sprost
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          id="basic-navbar-nav">
+          <Nav
+            className="me-auto">
             <Button
               variant="secondary"
               className="m-2"
+              disabled={currentView === <Account />}
               onClick={() => setCurrentView(<Account />)}>
               <PersonCircle
                 className="mx-2" />
@@ -30,6 +40,7 @@ const Navigation = () => {
             <Button
               variant="secondary"
               className="m-2"
+              disabled={currentView === <Dashboard />}
               onClick={() => setCurrentView(<Dashboard />)}>
               <Speedometer
                 className="mx-2" />
