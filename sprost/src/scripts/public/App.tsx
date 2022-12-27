adba8e56e44175dd31e6807b3b1957953753f1e6
Nathan Wick <nathanwick2001@gmail.com>
2022-12-27 18:00:44 -0500

    Update Public App

--- sprost/src/scripts/public/App.tsx
+++ sprost/src/scripts/public/App.tsx
@@ -1,46 +1,80 @@
-import { doc, Firestore, getDoc } from "firebase/firestore";
-import React, { FC, useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { App as AppType } from "../../types/App";
-import { DatabaseContext } from "../Database";
-import { UserContext } from "../User";
+
+import {Firestore, doc, getDoc} from "firebase/firestore";
+import React, {FC, useContext, useEffect, useState} from "react";
+import {App as AppType} from "../../types/App";
+import {DatabaseContext} from "../Database";
 import Navigation from "./components/Navigation";
+import {UserContext} from "../User";
 import View from "./View";
+import {useParams} from "react-router-dom";
 
-const App: FC<{ propsAppRoute?: string, propsViewRoute?: string }> = ({ propsAppRoute, propsViewRoute }) => {
-	const params = useParams();
-	const database = useContext(DatabaseContext);
-	const user = useContext(UserContext);
-	const [ app, setApp ] = useState<AppType | undefined>();
-	const view = propsViewRoute ? app?.views[app.views.findIndex(view => view.route === propsViewRoute)] : app?.views[0];
-	const getAppFromUser = () => {
-		setApp(user?.apps.find(userApp => userApp.route === propsAppRoute));
-	};
-	const getAppFromDatabase = async () => {
-		const paramsUserRoute = params.userRoute;
-		const paramsAppRoute = params.appRoute;
-		if (paramsUserRoute && paramsAppRoute) {
-			const appOwnerReference = paramsUserRoute ? doc(database as Firestore, "public", paramsUserRoute) : undefined;
-			const appOwnerSnapshot = appOwnerReference ? await getDoc(appOwnerReference) : undefined;
-			setApp(appOwnerSnapshot?.data()?.apps.find((app: { route: string | undefined; }) => app.route === paramsAppRoute));
-		}
-	};
+const App: FC<{
+    propsAppRoute?: string,
+    propsViewRoute?: string
+}> = ({propsAppRoute, propsViewRoute}) => {
 
-	useEffect(() => {
-		if (propsAppRoute) {
-			getAppFromUser();
-		} else {
-			getAppFromDatabase();
-		}
-	}, []);
+    const [
+            currentApp,
+            setCurrentApp
+        ] = useState<AppType | undefined>(),
+        currentView = propsViewRoute
+            ? currentApp?.views[
+                currentApp.views.findIndex((view) => view.route === propsViewRoute)
+            ]
+            : currentApp?.views[0],
+        database = useContext(DatabaseContext),
+        params = useParams(),
+        user = useContext(UserContext);
 
-	return <>{
-		app && view ?
-			<>
-				<Navigation app={app} />
-				<View view={view} />
-			</> : <>Loading...</>
-	}</>;
+    useEffect(
+        () => {
+
+            const getCurrentApp = async () => {
+
+                if (propsAppRoute) {
+
+                    setCurrentApp(user?.apps.find((userApp) => userApp.route === propsAppRoute));
+
+                } else {
+
+                    const paramsAppRoute = params.appRoute,
+                        paramsUserRoute = params.userRoute;
+
+                    if (paramsUserRoute && paramsAppRoute) {
+
+                        const appOwnerReference = doc(
+                                    database as Firestore,
+                                    "public",
+                                    paramsUserRoute
+                            ),
+                            appOwnerSnapshot = await getDoc(appOwnerReference),
+                            paramsUserApps = appOwnerSnapshot.data()?.apps;
+                        setCurrentApp(paramsUserApps.find((userApp: {
+                            route: string | undefined;
+                        }) => userApp.route === paramsAppRoute));
+
+                    }
+
+                }
+
+            };
+            getCurrentApp();
+
+        },
+        []
+    );
+
+    return <>{
+        currentApp && currentView
+            ? <>
+                <Navigation
+                    app={currentApp} />
+                <View
+                    view={currentView} />
+            </>
+            : <>Loading...</>
+    }</>;
+
 };
 
 export default App;
