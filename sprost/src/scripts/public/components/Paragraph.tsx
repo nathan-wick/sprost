85bd3d4f7aaa7d94cfe0044768fa9980100c6afc
Nathan Wick <nathanwick2001@gmail.com>
2022-12-27 16:11:22 -0500

    Update Public Paragraph

--- sprost/src/scripts/public/components/Paragraph.tsx
+++ sprost/src/scripts/public/components/Paragraph.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from "react";
-import { Paragraph as ParagraphType } from "../../../types/components/Paragraph";
+import React, {FC} from "react";
+import {
+    Paragraph as ParagraphType
+} from "../../../types/components/Paragraph";
 
-const Paragraph: FC<{ component: ParagraphType }> = ({ component }) => {
-	return <p>
-		{component.message}
-	</p>;
-};
+const Paragraph: FC<{component: ParagraphType}> = ({component}) => <p>
+    {component.message}
+</p>;
 
 export default Paragraph;
