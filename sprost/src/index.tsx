1d01dfd189e70dc122718c117969510d234e0257
Nathan Wick <nathanwick2001@gmail.com>
2022-12-28 17:46:09 -0500

    Update Index

--- sprost/src/index.tsx
+++ sprost/src/index.tsx
@@ -1,39 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
-import Firebase from "./scripts/Firebase";
+import "bootstrap/dist/css/bootstrap.css";
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import App from "./scripts/public/App";
 import Authentication from "./scripts/Authentication";
 import Database from "./scripts/Database";
-import User from "./scripts/User";
+import Firebase from "./scripts/Firebase";
 import Navigation from "./scripts/sprost/Navigation";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import App from "./scripts/public/App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import User from "./scripts/User";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-// TODO: Create a 404 and Loading view. Tutorial: https://reactrouter.com/en/main/start/tutorial
-root.render(
-	<React.StrictMode>
-		<Firebase>
-			<Database>
-				<Authentication>
-					<User>
-						<Router>
-							<Routes>
-								<Route
-									path="/"
-									element={<Navigation />}/>
-								<Route
-									path=":userRoute/:appRoute"
-									element={<App />}/>
-							</Routes>
-						</Router>
-					</User>
-				</Authentication>
-			</Database>
-		</Firebase>
-	</React.StrictMode>
-);
+root.render(<React.StrictMode>
+    <Firebase>
+        <Database>
+            <Authentication>
+                <User>
+                    <Router>
+                        <Routes>
+                            <Route
+                                path="/"
+                                element={<Navigation />}/>
+                            <Route
+                                path=":userRoute/:appRoute"
+                                element={<App />}/>
+                        </Routes>
+                    </Router>
+                </User>
+            </Authentication>
+        </Database>
+    </Firebase>
+</React.StrictMode>);
