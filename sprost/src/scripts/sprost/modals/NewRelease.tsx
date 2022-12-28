51d9e220b32cc0f7cd3ae9de78c63fdc560e8741
Nathan Wick <nathanwick2001@gmail.com>
2022-12-28 13:00:45 -0500

    Update NewRelease Modal

--- sprost/src/scripts/sprost/modals/NewRelease.tsx
+++ sprost/src/scripts/sprost/modals/NewRelease.tsx
@@ -1,176 +1,222 @@
-import { doc, Firestore, setDoc, updateDoc } from "firebase/firestore";
-import React, { FC, useContext, useState } from "react";
-import { Button, Form, FormGroup, Modal } from "react-bootstrap";
-import { BoxSeam, RocketTakeoffFill } from "react-bootstrap-icons";
-import { App } from "../../../types/App";
-import { User } from "../../../types/User";
-import { DatabaseContext } from "../../Database";
-import { UserContext } from "../../User";
+import {BoxSeam, RocketTakeoffFill} from "react-bootstrap-icons";
+import {Button, Form, FormGroup, Modal} from "react-bootstrap";
+import {Firestore, doc, setDoc, updateDoc} from "firebase/firestore";
+import React, {FC, useContext, useState} from "react";
+import {App} from "../../../types/App";
+import {DatabaseContext} from "../../Database";
+import {User} from "../../../types/User";
+import {UserContext} from "../../User";
 
 const NewRelease: FC<{
-	app: App | undefined,
-}> = ({
-	app,
-}) => {
-	const database = useContext(DatabaseContext);
-	const user = useContext(UserContext);
-	let version = structuredClone(app?.version);
-	const [ modal, setModal ] = useState<boolean>(false);
-	const [ releaseType, setReleaseType ] = useState<string>("minor");
-	const [ newVersion, setNewVersion ] = useState<{ major: number, minor: number, patch: number } | undefined>(version ? {
-		major: version.major,
-		minor: version.minor += 1,
-		patch: version.patch,
-	} : undefined);
-	const [ isLoading, setIsLoading ] = useState<boolean>(false);
-	const showModal = () => setModal(true);
-	const hideModal = () => reset();
+    app: App | undefined,
+}> = ({app}) => {
 
-	const releaseTypeOptions = [
-		{
-			value: "major",
-			text: "Major",
-		},
-		{
-			value: "minor",
-			text: "Minor",
-		},
-		{
-			value: "patch",
-			text: "Patch",
-		},
-	];
+    let version = structuredClone(app?.version);
+    const database = useContext(DatabaseContext),
+        user = useContext(UserContext),
+        [
+            modal,
+            setModal
+        ] = useState<boolean>(false),
+        [
+            releaseType,
+            setReleaseType
+        ] = useState<string>("minor"),
+        [
+            newVersion,
+            setNewVersion
+        ] = useState<{ major: number, minor: number, patch: number } | "undefined">(version
+            ? {
+                "major": version.major,
+                "minor": version.minor += 1,
+                "patch": version.patch
+            }
+            : "undefined"),
+        [
+            isLoading,
+            setIsLoading
+        ] = useState<boolean>(false),
+        releaseTypeOptions = [
+            {
+                "text": "Major",
+                "value": "major"
+            },
+            {
+                "text": "Minor",
+                "value": "minor"
+            },
+            {
+                "text": "Patch",
+                "value": "patch"
+            }
+        ],
+        onReleaseTypeChange = (event: { target: { value: string; }; }) => {
 
-	const onReleaseTypeChange = (event: { target: { value: string; }; }) => {
-		setReleaseType(event.target.value);
-		version = structuredClone(app?.version);
-		if (version) {
-			switch(event.target.value) {
-			case "major":
-				setNewVersion({
-					major: version.major += 1,
-					minor: version.minor,
-					patch: version.patch,
-				});
-				break;
-			case "minor":
-				setNewVersion({
-					major: version.major,
-					minor: version.minor += 1,
-					patch: version.patch,
-				});
-				break;
-			case "patch":
-			default:
-				setNewVersion({
-					major: version.major,
-					minor: version.minor,
-					patch: version.patch += 1,
-				});
-				break;
-			}
-		}
-	};
+            setReleaseType(event.target.value);
+            version = structuredClone(app?.version);
+            if (version) {
 
-	const reset = () => {
-		setReleaseType("minor");
-		version = structuredClone(app?.version);
-		setNewVersion(version ? {
-			major: version.major,
-			minor: version.minor += 1,
-			patch: version.patch,
-		} : undefined);
-		setIsLoading(false);
-		setModal(false);
-	};
+                switch (event.target.value) {
 
-	const onSubmit = async (event: { preventDefault: () => void; }) => {
-		setIsLoading(true);
-		event.preventDefault();
-		if (user && app && newVersion) {
-			const newApp = user.apps.find(newApp => newApp.route === app.route);
-			if (newApp) {
-				newApp.version = newVersion;
-				const userReference = doc(database as Firestore, "users", user.id);
-				await setDoc(userReference, user, { merge: true });
-				const publicUserReference = doc(database as Firestore, "public", user.route);
-				const newPublicUser: Partial<User> = {
-					apps: user.apps,
-				};
-				await updateDoc(publicUserReference, newPublicUser);
-			}
-		}
-		hideModal();
-	};
-	
-	return <>
-		<Button
-			className="w-50 shadow"
-			disabled={app?.views.length ? app.views.length < 1 : true}
-			variant="primary"
-			onClick={showModal}>
-			<RocketTakeoffFill
-				className="mx-2" />
+                case "major":
+                    setNewVersion({
+                        "major": version.major += 1,
+                        "minor": version.minor,
+                        "patch": version.patch
+                    });
+                    break;
+                case "minor":
+                    setNewVersion({
+                        "major": version.major,
+                        "minor": version.minor += 1,
+                        "patch": version.patch
+                    });
+                    break;
+                case "patch":
+                default:
+                    setNewVersion({
+                        "major": version.major,
+                        "minor": version.minor,
+                        "patch": version.patch += 1
+                    });
+                    break;
+
+                }
+
+            }
+
+        },
+        reset = () => {
+
+            setReleaseType("minor");
+            version = structuredClone(app?.version);
+            setNewVersion(version
+                ? {
+                    "major": version.major,
+                    "minor": version.minor += 1,
+                    "patch": version.patch
+                }
+                : "undefined");
+            setIsLoading(false);
+            setModal(false);
+
+        },
+        showModal = () => setModal(true),
+        hideModal = () => reset(),
+        onSubmit = async () => {
+
+            setIsLoading(true);
+            if (user && app && newVersion) {
+
+                const newApp = user.apps.find((newUserApp) => newUserApp.route === app.route);
+                if (newApp && newVersion !== "undefined") {
+
+                    newApp.version = newVersion;
+                    const userReference = doc(
+                            database as Firestore,
+                            "users",
+                            user.id
+                        ),
+                        publicUserReference = doc(
+                            database as Firestore,
+                            "public",
+                            user.route
+                        ),
+                        newPublicUser: Partial<User> = {
+                            "apps": user.apps
+                        };
+                    await setDoc(
+                        userReference,
+                        user,
+                        {"merge": true}
+                    );
+                    await updateDoc(
+                        publicUserReference,
+                        newPublicUser
+                    );
+
+                }
+
+            }
+            hideModal();
+
+        };
+
+    return <>
+        <Button
+            className="w-50 shadow"
+            disabled={app?.views.length
+                ? app.views.length < 1
+                : true}
+            variant="primary"
+            onClick={showModal}>
+            <RocketTakeoffFill
+                className="mx-2" />
             Release
-		</Button>
+        </Button>
 
-		<Modal 
-			show={modal}
-			onHide={hideModal}
-			className="text-dark">
-			<Modal.Header
-				className="bg-primary bg-gradient text-white">
-				<Modal.Title>
-					<RocketTakeoffFill
-						className="mx-2" />
-					Release
-				</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<Form>
-					<FormGroup>
-						<Form.Label>
-							<BoxSeam
-								className="mx-2" />
+        <Modal
+            show={modal}
+            onHide={hideModal}
+            className="text-dark">
+            <Modal.Header
+                className="bg-primary bg-gradient text-white">
+                <Modal.Title>
+                    <RocketTakeoffFill
+                        className="mx-2" />
+                    Release
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <FormGroup>
+                        <Form.Label>
+                            <BoxSeam
+                                className="mx-2" />
                             Release Size
-						</Form.Label>
-						<Form.Select
-							className={user?.theme.name === "dark" ? "bg-black text-light" : "bg-white text-dark"}
-							onChange={onReleaseTypeChange}
-							value={releaseType}>
-							{
-								releaseTypeOptions.map(releaseTypeOption =>
-									<option
-										key={releaseTypeOption.text}
-										value={releaseTypeOption.value}>
-										{releaseTypeOption.text}
-									</option>)
-							}
-						</Form.Select>
-					</FormGroup>
-				</Form>
-			</Modal.Body>
-			<Modal.Footer>
-				<Button
-					variant="outline-secondary"
-					className="m-2"
-					onClick={hideModal}>
-					Cancel
-				</Button>
-				<Button
-					variant="primary"
-					className="m-2"
-					disabled={isLoading}
-					onClick={onSubmit}>
-					{
-						isLoading ?
-							<>Loading...</> :
-							<>Release Version {newVersion?.major}.{newVersion?.minor}.{newVersion?.patch}</>
-					}
-				</Button>
-			</Modal.Footer>
-		</Modal>
-	</>;
+                        </Form.Label>
+                        <Form.Select
+                            className={user?.theme.name === "dark"
+                                ? "bg-black text-light"
+                                : "bg-white text-dark"}
+                            onChange={onReleaseTypeChange}
+                            value={releaseType}>
+                            {
+                                releaseTypeOptions.map((releaseTypeOption) => <option
+                                    key={releaseTypeOption.text}
+                                    value={releaseTypeOption.value}>
+                                    {releaseTypeOption.text}
+                                </option>)
+                            }
+                        </Form.Select>
+                    </FormGroup>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    variant="outline-secondary"
+                    className="m-2"
+                    onClick={hideModal}>
+                    Cancel
+                </Button>
+                <Button
+                    variant="primary"
+                    className="m-2"
+                    disabled={isLoading}
+                    onClick={onSubmit}>
+                    {
+                        isLoading || newVersion === "undefined"
+                            ? <>Loading...</>
+                            : <>
+                                Release Version {newVersion?.major}.
+                                {newVersion?.minor}.{newVersion?.patch}
+                            </>
+                    }
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    </>;
+
 };
 
 export default NewRelease;
