21ef45fde9955a3e4c28fc18e62a816bdff4a7c3
Nathan Wick <nathanwick2001@gmail.com>
2022-12-27 19:07:11 -0500

    Update Public View

--- sprost/src/scripts/public/View.tsx
+++ sprost/src/scripts/public/View.tsx
@@ -1,31 +1,35 @@
-import React, { FC } from "react";
-import { View as ViewType } from "../../types/View";
-import { Component as ComponentType } from "../../types/Component";
-import Header from "./components/Header";
-import Title from "./components/Title";
-import Paragraph from "./components/Paragraph";
 
-const View: FC<{ view: ViewType }> = ({ view }) => {
-	return <>
-		{
-			view?.components.map((component: ComponentType) => {
-				switch(component.type.id) {
-				case "header":
-					return <Header
-						key={component.id}
-						component={component.type}/>;
-				case "title":
-					return <Title
-						key={component.id}
-						component={component.type}/>;
-				case "paragraph":
-					return <Paragraph
-						key={component.id}
-						component={component.type}/>;
-				}
-			})
-		}
-	</>;
-};
+import React, {FC} from "react";
+import {Component as ComponentType} from "../../types/Component";
+import Header from "./components/Header";
+import Paragraph from "./components/Paragraph";
+import Title from "./components/Title";
+import {View as ViewType} from "../../types/View";
+
+const View: FC<{view: ViewType}> = ({view}) => <>
+    {
+        view?.components.map((component: ComponentType) => {
+
+            switch (component.type.id) {
+
+            case "header":
+                return <Header
+                    key={component.id}
+                    component={component.type}/>;
+            case "title":
+                return <Title
+                    key={component.id}
+                    component={component.type}/>;
+            case "paragraph":
+            default:
+                return <Paragraph
+                    key={component.id}
+                    component={component.type}/>;
+
+            }
+
+        })
+    }
+</>;
 
 export default View;
