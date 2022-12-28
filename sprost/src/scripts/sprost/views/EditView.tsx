699c928079c33751d1284ef0ae7aedf0d679ebea
Nathan Wick <nathanwick2001@gmail.com>
2022-12-28 14:23:05 -0500

    Update EditView View

--- sprost/src/scripts/sprost/views/EditView.tsx
+++ sprost/src/scripts/sprost/views/EditView.tsx
@@ -1,138 +1,189 @@
-import { doc, Firestore, setDoc } from "firebase/firestore";
-import React, { FC, useContext, useState } from "react";
-import { Button, ButtonGroup, Col, Row } from "react-bootstrap";
-import { ArrowsAngleContract, ArrowsAngleExpand, BoxArrowLeft, DeviceSsd, Window } from "react-bootstrap-icons";
-import { View as ViewType } from "../../../types/View";
-import { DatabaseContext } from "../../Database";
-import { UserContext } from "../../User";
-import Header from "../editors/Header";
-import Title from "../editors/Title";
-import Paragraph from "../editors/Paragraph";
-import NewComponent from "../modals/NewComponent";
-import { NavigationContext } from "../Navigation";
+import {ArrowsAngleContract, ArrowsAngleExpand, BoxArrowLeft, DeviceSsd,
+    Window} from "react-bootstrap-icons";
+import {Button, ButtonGroup, Col, Row} from "react-bootstrap";
+import {Firestore, doc, setDoc} from "firebase/firestore";
+import React, {FC, useContext, useState} from "react";
+import {DatabaseContext} from "../../Database";
 import EditApp from "./EditApp";
+import Header from "../editors/Header";
+import {NavigationContext} from "../Navigation";
+import NewComponent from "../modals/NewComponent";
+import Paragraph from "../editors/Paragraph";
+import Title from "../editors/Title";
+import {User} from "../../../types/User";
+import {UserContext} from "../../User";
 import View from "../../public/View";
+import {View as ViewType} from "../../../types/View";
 
-const EditView: FC<{ appRoute: string, viewRoute: string }> = ({ appRoute, viewRoute }) => {
-	const database = useContext(DatabaseContext);
-	const user = useContext(UserContext);
-	const { setCurrentView } = useContext(NavigationContext);
-	const app = user?.apps.find(app => app.route === appRoute);
-	const savedView = app?.views.find(view => view.route === viewRoute);
-	const [ editView, setEditView ] = useState<ViewType | undefined>(structuredClone(savedView));
-	const [ isSaving, setIsSaving ] = useState<boolean>(false);
-	const [ previewIsExpanded, setPreviewIsExpanded ] = useState<boolean>(false);
+const EditView: FC<{appRoute: string, viewRoute: string}> = ({appRoute, viewRoute}) => {
 
-	const exit = () => {
-		setCurrentView(<EditApp appRoute={String(appRoute)} />);
-	};
-	
-	const saveUser = async () => {
-		setIsSaving(true);
-		if (user) {
-			const newView = structuredClone(editView);
-			newView.isSaved = true;
-			const newUser = structuredClone(user);
-			const newUserAppViews = newUser.apps.find((app: { route: string; }) => app.route === appRoute).views;
-			const newUserAppViewIndex = newUserAppViews.findIndex((view: { route: string; }) => view.route === viewRoute);
-			newUserAppViews[newUserAppViewIndex] = newView;
-			const userReference = doc(database as Firestore, "users", user.id);
-			await setDoc(userReference, newUser, { merge: true });
-			setEditView(newView);
-		}
-		setIsSaving(false);
-	};
-    
-	return <>
-		<Row
-			className="gx-0 m-5">
-			<Col
-				lg={6}
-				md={4}
-				sm={12}>
-				<h1>
-					<Window
-						className="mx-2" />
-					{editView?.name}
-				</h1>
-			</Col>
-			<Col
-				className="p-2">
-				<ButtonGroup
-					className="w-100 shadow">
-					<Button
-						variant={editView?.isSaved ? "primary" : "outline-danger"}
-						onClick={exit}>
-						<BoxArrowLeft
-							className="mx-2" />
-						Exit
-					</Button>
-					<Button
-						variant={"outline-primary"}
-						onClick={() => setPreviewIsExpanded(!previewIsExpanded)}>
-						{
-							previewIsExpanded ?
-								<ArrowsAngleContract
-									className="mx-2" /> :
-								<ArrowsAngleExpand
-									className="mx-2" />
-						}
-						Preview
-					</Button>
-					<Button
-						variant={editView?.isSaved ? "outline-success" : "primary"}
-						disabled={isSaving || editView?.isSaved}
-						onClick={saveUser}>
-						<DeviceSsd
-							className="mx-2" />
-						{isSaving ? "Saving..." : editView?.isSaved ? "Saved" : "Save"}
-					</Button>
-				</ButtonGroup>
-			</Col>
-		</Row>
-		<Row
-			className="gx-0 m-5">
-			<Col
-				md={previewIsExpanded ? 12 : 6}
-				sm={12}
-				className={previewIsExpanded ? "" : "d-none d-lg-block"}>
-				<div
-					className="m-2 mb-4 rounded shadow">
-					{
-						editView &&
-							<View view={editView} />
-					}
-				</div>
-			</Col>
-			<Col>
-				<Row
-					className="gx-0">
-					<Col
-						className="p-2">
-						<NewComponent editView={editView} setEditView={setEditView} />
-					</Col>
-				</Row>
-				{
-					editView?.components.map((component, index) => 
-						<div
-							key={index}>
-							{(() => {
-								switch (component.type.id) {
-								case "header":
-									return <Header componentId={component.id} editView={editView} setEditView={setEditView} />;
-								case "title":
-									return <Title componentId={component.id} editView={editView} setEditView={setEditView} />;
-								case "paragraph":
-									return <Paragraph componentId={component.id} editView={editView} setEditView={setEditView} />;
-								default:
-									return <></>;
-								}
-							})()}
-						</div>)
-				}
-			</Col>
-		</Row>
-	</>;
+    const database = useContext(DatabaseContext),
+        user = useContext(UserContext),
+        {setCurrentView} = useContext(NavigationContext),
+        app = user?.apps.find((userApp) => userApp.route === appRoute),
+        savedView = app?.views.find((view) => view.route === viewRoute),
+        [
+            editView,
+            setEditView
+        ] = useState<ViewType | undefined>(structuredClone(savedView)),
+        [
+            isSaving,
+            setIsSaving
+        ] = useState<boolean>(false),
+        [
+            previewIsExpanded,
+            setPreviewIsExpanded
+        ] = useState<boolean>(false),
+        exit = () => {
+
+            setCurrentView(<EditApp appRoute={String(appRoute)} />);
+
+        },
+        saveUser = async () => {
+
+            setIsSaving(true);
+            if (user) {
+
+                const newView: ViewType = structuredClone(editView),
+                    newUser: User = structuredClone(user),
+                    newUserAppViews = newUser.apps.find((userApp: {
+                        route: string;
+                    }) => userApp.route === appRoute)?.views,
+                    newUserAppViewIndex = newUserAppViews?.findIndex((view: {
+                        route: string;
+                    }) => view.route === viewRoute),
+                    userReference = doc(
+                        database as Firestore,
+                        "users",
+                        user.id
+                    );
+                if (newUserAppViews && newUserAppViewIndex) {
+
+                    newUserAppViews[newUserAppViewIndex] = newView;
+                    newView.isSaved = true;
+                    await setDoc(
+                        userReference,
+                        newUser,
+                        {"merge": true}
+                    );
+                    setEditView(newView);
+
+                }
+
+            }
+            setIsSaving(false);
+
+        };
+
+    return <>
+        <Row
+            className="gx-0 m-5">
+            <Col
+                lg={6}
+                md={4}
+                sm={12}>
+                <h1>
+                    <Window
+                        className="mx-2" />
+                    {editView?.name}
+                </h1>
+            </Col>
+            <Col
+                className="p-2">
+                <ButtonGroup
+                    className="w-100 shadow">
+                    <Button
+                        variant={editView?.isSaved
+                            ? "primary"
+                            : "outline-danger"}
+                        onClick={exit}>
+                        <BoxArrowLeft
+                            className="mx-2" />
+                        Exit
+                    </Button>
+                    <Button
+                        variant={"outline-primary"}
+                        onClick={() => setPreviewIsExpanded(!previewIsExpanded)}>
+                        {
+                            previewIsExpanded
+                                ? <ArrowsAngleContract
+                                    className="mx-2" />
+                                : <ArrowsAngleExpand
+                                    className="mx-2" />
+                        }
+                        Preview
+                    </Button>
+                    <Button
+                        variant={editView?.isSaved
+                            ? "outline-success"
+                            : "primary"}
+                        disabled={isSaving || editView?.isSaved}
+                        onClick={saveUser}>
+                        <DeviceSsd
+                            className="mx-2" />
+                        {isSaving
+                            ? "Saving..."
+                            : editView?.isSaved
+                                ? "Saved"
+                                : "Save"}
+                    </Button>
+                </ButtonGroup>
+            </Col>
+        </Row>
+        <Row
+            className="gx-0 m-5">
+            <Col
+                md={previewIsExpanded
+                    ? 12
+                    : 6}
+                sm={12}
+                className={previewIsExpanded
+                    ? ""
+                    : "d-none d-lg-block"}>
+                <div
+                    className="m-2 mb-4 rounded shadow">
+                    {
+                        editView &&
+                            <View view={editView} />
+                    }
+                </div>
+            </Col>
+            <Col>
+                <Row
+                    className="gx-0">
+                    <Col
+                        className="p-2">
+                        <NewComponent editView={editView} setEditView={setEditView} />
+                    </Col>
+                </Row>
+                {
+                    editView?.components.map((component, index) => <div
+                        key={index}>
+                        {(() => {
+
+                            switch (component.type.id) {
+
+                            case "header":
+                                return <Header componentId={component.id} editView={editView}
+                                    setEditView={setEditView} />;
+                            case "title":
+                                return <Title componentId={component.id} editView={editView}
+                                    setEditView={setEditView} />;
+                            case "paragraph":
+                                return <Paragraph componentId={component.id} editView={editView}
+                                    setEditView={setEditView} />;
+                            default:
+                                return <></>;
+
+                            }
+
+                        })()}
+                    </div>)
+                }
+            </Col>
+        </Row>
+    </>;
+
 };
 
 export default EditView;
