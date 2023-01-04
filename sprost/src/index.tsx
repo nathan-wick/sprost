30cae5daa2cdea9b1c818ad324ff19f75e90c5c8
Nathan Wick <nathanwick2001@gmail.com>
2023-01-03 19:26:33 -0500

    Renamed Scripts Folder To Components

--- sprost/src/index.tsx
+++ sprost/src/index.tsx
@@ -1,13 +1,13 @@
 import "./custom.scss";
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
-import App from "./scripts/public/App";
-import Authentication from "./scripts/Authentication";
-import Database from "./scripts/Database";
-import Firebase from "./scripts/Firebase";
-import Navigation from "./scripts/sprost/Navigation";
+import App from "./components/public/App";
+import Authentication from "./components/Authentication";
+import Database from "./components/Database";
+import Firebase from "./components/Firebase";
+import Navigation from "./components/sprost/Navigation";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import User from "./scripts/User";
+import User from "./components/User";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
