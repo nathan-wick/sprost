5af92bf6ca42b14a779fb7b27ceb45dc4adaac27
Nathan Wick <nathanwick2001@gmail.com>
2023-01-18 19:22:56 -0500

    Update Paragraph

--- sprost/src/components/generated/components/Paragraph.tsx
+++ sprost/src/components/generated/components/Paragraph.tsx
@@ -1,10 +1,18 @@
+import {Col, Row} from "react-bootstrap";
 import React, {FC} from "react";
 import {
     Paragraph as ParagraphType
 } from "../../../types/components/Paragraph";
 
-const Paragraph: FC<{component: ParagraphType}> = ({component}) => <p>
-    {component.message}
-</p>;
+const Paragraph: FC<{component: ParagraphType}> = ({component}) => <Row
+    className="gx-0 d-flex justify-content-md-center p-4">
+    <Col
+        md={6}
+        sm={12}>
+        <p>
+            {component.message}
+        </p>
+    </Col>
+</Row>;
 
 export default Paragraph;
